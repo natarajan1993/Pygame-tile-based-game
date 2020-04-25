09e11c633f8f704cb0f7b71427e0c5da1ec17cad
Nate <natraj_93@yahoo.com>
2020-04-24 22:31:32 -0500

    1 - Added helper function to load map from text file 2 - Added basic collision checking between walls and player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+from os import path
 from settings import *
 from sprites import *
 
@@ -13,15 +14,23 @@ class Game:
         self.load_data()
 
     def load_data(self):
-        pass
+        """Load map data from a text file and append it to the map data text file"""
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
 
     def new(self):
         # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        self.player = Player(self, 10, 10)
-        for x in range(10, 20):
-            Wall(self, x, 5)
+        for row, tiles in enumerate(self.map_data):
+            for col,tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
 
     def run(self):
         # game loop - set self.playing = False to end the game
